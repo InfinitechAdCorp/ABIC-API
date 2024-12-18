a081da8f19d1bfc83612a4dd9a6765e646cc2ef6
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2024-12-18 16:01:58 +0800

    remove middleware

--- routes/api.php
+++ routes/api.php
@@ -30,66 +30,66 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/get', [UserController::class, 'get']);
         Route::post('/logout', [UserController::class, 'logout']);
     });
+});
 
-    Route::prefix('questions')->group(function () {
-        Route::get('', [QuestionController::class, 'getAll']);
-        Route::get('{id}', [QuestionController::class, 'get']);
-        Route::post('', [QuestionController::class, 'create']);
-        Route::put('', [QuestionController::class, 'update']);
-        Route::delete('{id}', [QuestionController::class, 'delete']);
-    });
+Route::prefix('questions')->group(function () {
+    Route::get('', [QuestionController::class, 'getAll']);
+    Route::get('{id}', [QuestionController::class, 'get']);
+    Route::post('', [QuestionController::class, 'create']);
+    Route::put('', [QuestionController::class, 'update']);
+    Route::delete('{id}', [QuestionController::class, 'delete']);
+});
 
-    Route::prefix('properties')->group(function () {
-        Route::get('', [PropertyController::class, 'getAll']);
-        Route::get('{id}', [PropertyController::class, 'get']);
-        Route::post('', [PropertyController::class, 'create']);
-        Route::put('', [PropertyController::class, 'update']);
-        Route::delete('{id}', [PropertyController::class, 'delete']);
-    });
+Route::prefix('properties')->group(function () {
+    Route::get('', [PropertyController::class, 'getAll']);
+    Route::get('{id}', [PropertyController::class, 'get']);
+    Route::post('', [PropertyController::class, 'create']);
+    Route::put('', [PropertyController::class, 'update']);
+    Route::delete('{id}', [PropertyController::class, 'delete']);
+});
 
-    Route::prefix('amenities')->group(function () {
-        Route::get('', [AmenityController::class, 'getAll']);
-        Route::get('{id}', [AmenityController::class, 'get']);
-        Route::post('', [AmenityController::class, 'create']);
-        Route::put('', [AmenityController::class, 'update']);
-        Route::delete('{id}', [AmenityController::class, 'delete']);
-    });
+Route::prefix('amenities')->group(function () {
+    Route::get('', [AmenityController::class, 'getAll']);
+    Route::get('{id}', [AmenityController::class, 'get']);
+    Route::post('', [AmenityController::class, 'create']);
+    Route::put('', [AmenityController::class, 'update']);
+    Route::delete('{id}', [AmenityController::class, 'delete']);
+});
 
-    Route::prefix('property-submissions')->group(function () {
-        Route::get('', [PropertySubmissionController::class, 'getAll']);
-        Route::get('{id}', [PropertySubmissionController::class, 'get']);
-        Route::post('', [PropertySubmissionController::class, 'store']);
-        Route::put('{id}', [PropertySubmissionController::class, 'update']);
-        Route::delete('{id}', [PropertySubmissionController::class, 'delete']);
-    });
+Route::prefix('property-submissions')->group(function () {
+    Route::get('', [PropertySubmissionController::class, 'getAll']);
+    Route::get('{id}', [PropertySubmissionController::class, 'get']);
+    Route::post('', [PropertySubmissionController::class, 'store']);
+    Route::put('{id}', [PropertySubmissionController::class, 'update']);
+    Route::delete('{id}', [PropertySubmissionController::class, 'delete']);
+});
 
-    Route::prefix('certificates')->group(function () {
-        Route::get('', [CertificateController::class, 'getAll']);
-        Route::get('{id}', [CertificateController::class, 'get']);
-        Route::post('', [CertificateController::class, 'create']);
-        Route::put('', [CertificateController::class, 'update']);
-        Route::delete('{id}', [CertificateController::class, 'delete']);
-    });
+Route::prefix('certificates')->group(function () {
+    Route::get('', [CertificateController::class, 'getAll']);
+    Route::get('{id}', [CertificateController::class, 'get']);
+    Route::post('', [CertificateController::class, 'create']);
+    Route::put('', [CertificateController::class, 'update']);
+    Route::delete('{id}', [CertificateController::class, 'delete']);
+});
 
-    Route::prefix('schedules')->group(function () {
-        Route::get('', [ScheduleController::class, 'getAll']);
-        Route::get('{id}', [ScheduleController::class, 'get']);
-        Route::post('', [ScheduleController::class, 'create']);
-        Route::put('', [ScheduleController::class, 'update']);
-        Route::delete('{id}', [ScheduleController::class, 'delete']);
+Route::prefix('schedules')->group(function () {
+    Route::get('', [ScheduleController::class, 'getAll']);
+    Route::get('{id}', [ScheduleController::class, 'get']);
+    Route::post('', [ScheduleController::class, 'create']);
+    Route::put('', [ScheduleController::class, 'update']);
+    Route::delete('{id}', [ScheduleController::class, 'delete']);
 
-        Route::post('/change-status', [ScheduleController::class, 'changeStatus']);
-    });
+    Route::post('/change-status', [ScheduleController::class, 'changeStatus']);
+});
 
-    Route::prefix('testimonials')->group(function () {
-        Route::get('', [TestimonialController::class, 'getAll']);
-        Route::get('{id}', [TestimonialController::class, 'get']);
-        Route::post('', [TestimonialController::class, 'create']);
-        Route::put('', [TestimonialController::class, 'update']);
-        Route::delete('{id}', [TestimonialController::class, 'delete']);
+Route::prefix('testimonials')->group(function () {
+    Route::get('', [TestimonialController::class, 'getAll']);
+    Route::get('{id}', [TestimonialController::class, 'get']);
+    Route::post('', [TestimonialController::class, 'create']);
+    Route::put('', [TestimonialController::class, 'update']);
+    Route::delete('{id}', [TestimonialController::class, 'delete']);
 
-        Route::post('/change-status', [TestimonialController::class, 'changeStatus']);
-    });
+    Route::post('/change-status', [TestimonialController::class, 'changeStatus']);
 });
 
 Route::prefix('users')->group(function () {
