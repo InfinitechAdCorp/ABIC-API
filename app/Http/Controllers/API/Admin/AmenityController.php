92e8725177989f76256167f971a4b696bcedf8a3
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-01-21 10:23:44 +0800

    amenities

---
+++ app/Http/Controllers/API/Admin/AmenityController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Amenity as Model;
+
+class AmenityController extends Controller
+{
+    public $model = "Amenity";
+
+    public function getAll()
+    {
+        $records = Model::with('property')->orderBy('updated_at', 'desc')->get();
+        $code = 200;
+        $response = ['message' => "Fetched Amenities", 'records' => $records];
+        return response()->json($response, $code);
+    }
+
+    public function get($id)
+    {
+        $record = Model::with('property')->where('id', $id)->get();
+        if ($record) {
+            $code = 200;
+            $response = ['message' => "Fetched $this->model", 'record' => $record];
+        }
+        else {
+            $code = 404;
+            $response = ['message' => "$this->model Not Found"];
+        }
+        return response()->json($response, $code);
+    }
+
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'property_id' => 'required|exists:properties,id',
+            'name' => 'required',
+        ]);
+
+        $record = Model::create($validated);
+        $code = 201;
+        $response = ['message' => "Created $this->model", 'record' => $record];
+        return response()->json($response, $code);
+    }
+
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'id' => 'required|exists:amenities,id',
+            'property_id' => 'required|exists:properties,id',
+            'name' => 'required',
+        ]);
+
+        $record = Model::find($validated['id']);
+        $record->update($validated);
+        $code = 200;
+        $response = ['message' => "Updated $this->model", 'record' => $record];
+        return response()->json($response, $code);
+    }
+
+    public function delete($id)
+    {
+        $record = Model::find($id);
+        if ($record) {
+            $record->delete();
+            $code = 200;
+            $response = ['message' => "Deleted $this->model"];
+        }
+        else {
+            $code = 404;
+            $response = ['message' => "$this->model Not Found"];
+        }
+        return response($response, $code);
+    }
+}
---
+++ app/Models/Amenity.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Amenity extends Model
+{
+    use HasFactory;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'property_id',
+        'name',
+    ];
+
+    public static function booted()
+    {
+        static::creating(function (Amenity $record) {
+            $record->id = Str::ulid();
+        });
+    }
+
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class);
+    }
+}
