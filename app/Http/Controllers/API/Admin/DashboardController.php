89dedfbc18d648de93dc6ee324ad8cde2b631fc3
tnmtthw <118366103+tnmtthw@users.noreply.github.com>
2025-01-30 14:33:31 +0800

    Dashboard final

--- app/Http/Controllers/API/Admin/DashboardController.php
+++ app/Http/Controllers/API/Admin/DashboardController.php
@@ -5,45 +5,37 @@ namespace App\Http\Controllers\API\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\Property;
-use App\Models\Listing;
-use App\Models\Appointment;
-use App\Models\Inquiry;
-use App\Models\Application;
-use App\Models\Plan;
-use App\Models\Question;
 use App\Models\Article;
+use App\Models\Career;
+use App\Models\Inquiry;
+use App\Models\Item;
+use App\Models\Partner;
+use App\Models\Property;
+use App\Models\Schedule;
+use App\Models\Testimonial;
 
 class DashboardController extends Controller
 {
     public function getCounts()
     {
-        $properties = Property::get()->count();
-        $rfoProperties = Property::where('status', 'Ready For Occupancy')->get()->count();
-        $underConstructionProperties = Property::where('status', 'Under Construction')->get()->count();
-        $newProperties = Property::where('status', 'New')->get()->count();
-        $listings = Listing::get()->count();
-        $inquiries = Inquiry::get()->count();
-        $viewings = Appointment::get()->count();
-        $applications = Application::get()->count();
-        $plans = Plan::get()->count();
-        $questions = Question::get()->count();
-        $article = Article::get()->count();
-
+        $article = Article::count();
+        $career = Career::count();
+        $inquiry = Inquiry::count();
+        $item = Item::count();
+        $partner = Partner::count();
+        $property = Property::count();
+        $testimonial = Testimonial::count();
 
         $records = [
-            'properties' => $properties,
-            'rfoProperties' => $rfoProperties,
-            'underConstructionProperties' => $underConstructionProperties,
-            'newProperties' => $newProperties,
-            'listings' => $listings,
-            'inquiries' => $inquiries,
-            'viewings' => $viewings,
-            'applications' => $applications,
-            'plans' => $plans,
-            'questions' => $questions,
-            'article' => $article,
+            'articles' => $article,
+            'careers' => $career,
+            'inquiries' => $inquiry,
+            'items' => $item,
+            'partners' => $partner,
+            'properties' => $property,
+            'testimonials' => $testimonial,
         ];
+        
         $code = 200;
         $response = ['message' => "Fetched Counts", 'records' => $records];
         return response()->json($response, $code);
